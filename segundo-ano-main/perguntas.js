3a789c0b671275c81615ad2af3d0056357f64d42
IsaGrandi <143717488+IsaGrandi@users.noreply.github.com>
2024-10-16 09:17:45 -0300

    Add files via upload

---
+++ segundo-ano-main/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Inglês',
+    'Como dizer "bom dia" em inglês?',
+    'Good morning'
+)
+
+criaCartao(
+    'Espanhol',
+    'Como dizer "boa tarde" em espanhol?',
+    'Buenas tardes'
+)
+
+criaCartao(
+    'Italiano',
+    'Como dizer "boa noite" em italiano?',
+    'Buonasera/ buona notte'
+)
+
+criaCartao(
+    'Francês',
+    'Como dizer "bom dia/ olá" em francês?',
+    'Bonjuor'
+)
